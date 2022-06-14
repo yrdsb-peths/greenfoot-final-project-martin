1f932b162cce1f111ab40f917465c44a3cc351d3
Martin Baldwin <340821446@gapps.yrdsb.ca>
2022-06-14 10:45:08 -0400

    Make coins fade away over time

--- Coin.java
+++ Coin.java
@@ -7,6 +7,13 @@ import greenfoot.*;
  * @version June 2022
  */
 public class Coin extends AnimatedActor {
+	private static final int LIFESPAN = 7500;
+	// Number of steps to decrease 'transparency' (really opacity) each act cycle while fading away
+	private static final int FADE_INTERVAL = 4;
+
+	private int transparency = 255;
+	private SimpleTimer timer = new SimpleTimer();
+
 	private static boolean hasLoadedImages = false;
 	private static final GreenfootImage[] images = new GreenfootImage[2];
 
@@ -29,5 +36,25 @@ public class Coin extends AnimatedActor {
 			images[i] = new GreenfootImage("images/coin-" + i + ".png");
 		}
 		hasLoadedImages = true;
+	}
+
+	/**
+	 * Check if it is time to fade or remove this coin.
+	 */
+	public void act() {
+		updateAnimation();
+		if (timer.millisElapsed() >= LIFESPAN) {
+			// Gradually decrease the image 'transparency' (really opacity) before removing this coin
+			transparency -= FADE_INTERVAL;
+			if (transparency <= 0) {
+				// Remove this coin once it has become fully transparent
+				getWorld().removeObject(this);
+				return;
+			}
+			// Set the transparency on a copy of the original image so the change is not permanent
+			GreenfootImage image = new GreenfootImage(getImage());
+			image.setTransparency(transparency);
+			setImage(image);
+		}
 	}
 }
